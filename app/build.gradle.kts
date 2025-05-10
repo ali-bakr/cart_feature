651f2b408e6c68a46afd45cfc0fca46957c1b0eb
Ali Abou-bakr <devabakr@gmail.com>
2025-05-10 14:40:11 +0300

    Module cart added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.aliaboubakr.superapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.aliaboubakr.superapp"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Super App"
 include(":app")
- 
+include(":cart_feature")
